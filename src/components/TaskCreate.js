fbc82972cc96a16babacc921f1aa8fd1148ed5f3
Volkan Abaoglu <abaoglu.2002@gmail.com>
2024-01-03 22:11:28 +0300

    Added CSS for edit section

--- src/components/TaskCreate.js
+++ src/components/TaskCreate.js
@@ -1,36 +1,58 @@
-import "../styles/taskCreate.css"
+import "../styles/taskCreate.css";
 import { useState } from "react";
 
+function TaskCreate({ onCreate, task, editState }) {
+  const [title, setTitle] = useState(editState ? task.title : "");
+  const [content, setContent] = useState(editState ? task.content : "");
 
-function TaskCreate({onCreate , task , editState}) {
-    const [title, setTitle] = useState(editState ? task.title : "");
-    const [content, setContent] = useState(editState ? task.content : "");
+  const handleChangeTitle = (e) => {
+    setTitle(e.target.value);
+  };
 
-    const handleChangeTitle = (e) =>{
-        setTitle(e.target.value);
-    }
+  const handleChangeContent = (e) => {
+    setContent(e.target.value);
+  };
 
-    const handleChangeContent  = (e) =>{
-        setContent(e.target.value)
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onCreate(title, content);
+    setTitle("");
+    setContent("");
+  };
 
-    const handleSubmit = (e) =>{
-        e.preventDefault();
-        onCreate(title,content);
-        setTitle("");
-        setContent("");
-    }
-
-    return ( <div className="task-form-all">
-        <h3 className="app-h3">Enter a Task !</h3>
-        <form className="task-form">
+  return (
+    <>
+      {editState ? (
+        <div>
+          <div className="task-form-all">
+            <h3 className="app-h3">Edit Task !</h3>
+            <form className="task-edit-form">
+              <label>Edit Title</label>
+              <input value={title} onChange={handleChangeTitle} />
+              <label>Edit Content</label>
+              <textarea value={content} onChange={handleChangeContent} />
+              <button className="task-edit-button" onClick={handleSubmit}>
+                OK
+              </button>
+            </form>
+          </div>
+        </div>
+      ) : (
+        <div className="task-form-all">
+          <h3 className="app-h3">Enter a Task !</h3>
+          <form className="task-form">
             <label>Title</label>
-            <input value={title} onChange={handleChangeTitle}/>
+            <input value={title} onChange={handleChangeTitle} />
             <label>Content</label>
             <textarea value={content} onChange={handleChangeContent} />
-            <button className="task-create-button" onClick={handleSubmit} >Create</button>
-        </form>
-    </div> );
+            <button className="task-create-button" onClick={handleSubmit}>
+              Create
+            </button>
+          </form>
+        </div>
+      )}
+    </>
+  );
 }
 
 export default TaskCreate;
